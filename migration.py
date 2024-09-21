2bb9b3cc62dbea87389a38d4d35db0d9668a309c
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2024-09-21 13:46:09 +0530

    added logs and refactor

--- migration.py
+++ migration.py
@@ -3,9 +3,12 @@ import hashlib
 import json
 import os
 import uuid
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 PATH_TO_BLOGS = "/Users/vicky/Repository/Blog-Scratch/blogs"
-PATH_TO_STATIC = "/_generated"
 
 METADATA_FILE_NAME = "metadata.json"
 
@@ -14,6 +17,16 @@ BLOG_FILE_NAME = "blog.mdx"
 # read the blog and update metadata
 
 directory_with_blogs = os.fsencode(PATH_TO_BLOGS)
+
+
+def is_valid_uuid(uuid_to_test, version=4):
+    try:
+        # check for validity of Uuid
+        uuid_obj = uuid.UUID(uuid_to_test, version=version)
+    except ValueError:
+        print('not a valid uuid {}'.format(uuid_to_test))
+        return 'false';
+    return 'true'
 
 
 def hashfile(file):
@@ -54,69 +67,62 @@ def hashfile(file):
     return sha256.hexdigest()
 
 
-# create the static data post processing
-for directory in os.listdir(directory_with_blogs):
-    blog_dir_name = os.fsdecode(directory)
+def main():
+    for directory in os.listdir(directory_with_blogs):
+        blog_dir_name = os.fsdecode(directory)
 
-    current_time = datetime.datetime.now()
+        current_time = datetime.datetime.now()
 
-    if len(blog_dir_name.split(":")) == 2:
-        blog_number = blog_dir_name.split(":")[0]
-        blog_name = blog_dir_name.split(":")[1]
-        blog_metadata_path = os.path.join(PATH_TO_BLOGS, blog_dir_name, METADATA_FILE_NAME)
-        with (open(blog_metadata_path) as metadata_json_file):
-            metadata = json.load(metadata_json_file)
+        if not os.path.isdir(os.path.join(PATH_TO_BLOGS, blog_dir_name)):
+            continue
 
-            if ("publish" not in metadata) or ("blogNumber" not in metadata) or ("previewImageSrc" not in metadata) or (
-                    "author" not in metadata) or ("tags" not in metadata) or ("title" not in metadata) or (
-                    "summary" not in metadata) or ("slug" not in metadata) or ("demo" not in metadata):
-                print('missing mandatory data in blog {}'.format(blog_dir_name))
-                break
+        if is_valid_uuid(blog_dir_name) == 'true':
+            logging.info("Processing blog {}".format(blog_dir_name))
+            blog_metadata_path = os.path.join(PATH_TO_BLOGS, blog_dir_name, METADATA_FILE_NAME)
+            with (open(blog_metadata_path) as metadata_json_file):
+                metadata = json.load(metadata_json_file)
 
-            blog_content_path = os.path.join(PATH_TO_BLOGS, blog_dir_name, BLOG_FILE_NAME)
-            blog_content_file_hash = hashfile(blog_content_path)
-            metadata_content_file_hash = hashfile(blog_metadata_path)
+                if ("publish" not in metadata) or ("blogNumber" not in metadata) or (
+                        "previewImageSrc" not in metadata) or (
+                        "author" not in metadata) or ("tags" not in metadata) or ("title" not in metadata) or (
+                        "summary" not in metadata) or ("slug" not in metadata) or ("demo" not in metadata):
+                    logging.warning('missing mandatory data in blog {} so not processing'.format(blog_dir_name))
+                    break
 
-            if "id" in metadata:
-                if blog_content_file_hash == metadata["hash"]:
-                    print(
-                        'Blog metadata has no changes from {} so skipping update of {} {}'.format(metadata["updatedAt"],
-                                                                                                  blog_name, blog_number))
+                blog_number = metadata["blogNumber"]
+                blog_name = metadata["title"]
+
+                blog_content_path = os.path.join(PATH_TO_BLOGS, blog_dir_name, BLOG_FILE_NAME)
+                blog_content_file_hash = hashfile(blog_content_path)
+                metadata_content_file_hash = hashfile(blog_metadata_path)
+
+                if "hash" in metadata:
+                    if blog_content_file_hash == metadata["hash"]:
+                        logging.info(
+                            'Blog metadata has no changes from {} so skipping update of {} {}'.format(
+                                metadata["updatedAt"],
+                                blog_name, blog_number))
+                    else:
+                        logging.info(
+                            'Blog content changed from {} so updating {} {}'.format(metadata["updatedAt"], blog_name,
+                                                                                    blog_number))
+                        metadata["updatedAt"] = str(current_time)
+                        metadata["hash"] = blog_content_file_hash
+                        metadata["version"] = metadata["version"] + 1
+                        json.dump(metadata, open(blog_metadata_path, "w"), ensure_ascii=False, indent=4, sort_keys=True)
+
                 else:
-                    print('Blog content changed from {} so updating {} {}'.format(metadata["updatedAt"], blog_name,
-                                                                                  blog_number))
+                    logging.info('No hash so processing the new blog {} {} by {}'.format(blog_name, blog_number,
+                                                                                         metadata["author"]))
+
+                    metadata["version"] = 1
                     metadata["updatedAt"] = str(current_time)
+                    metadata["createdAt"] = str(current_time)
                     metadata["hash"] = blog_content_file_hash
-                    metadata["version"] = metadata["version"] + 1
 
-                    with open(blog_content_path, 'r') as blog_content_file:
-                        blog_content = blog_content_file.read()
-                        processed_blog_content_file_path = os.path.join(PATH_TO_STATIC, metadata["id"],
-                                                                        metadata["id"] + ".mdx")
-                        with open(processed_blog_content_file_path, 'w') as processed_blog_content_file:
-                            processed_blog_content_file.write(blog_content)
+                    json.dump(metadata, open(blog_metadata_path, "w"), ensure_ascii=False, indent=4, sort_keys=True)
+        else:
+            logging.info('Blog directory name {} does not have the correct format so skipping'.format(blog_dir_name))
 
-                json.dump(metadata, open(blog_metadata_path, "w"), ensure_ascii=False, indent=4, sort_keys=True)
-                processed_metadata_file_path = os.path.join(PATH_TO_STATIC, metadata["id"], metadata["id"] + ".json")
-                json.dump(metadata, open(processed_metadata_file_path, "w"), ensure_ascii=False, indent=4, sort_keys=True)
-            else:
-                print('No hash so processing the new blog {} {} by {}'.format(blog_name, blog_number, metadata["author"]))
-                metadata["version"] = 1
-                metadata["updatedAt"] = str(current_time)
-                metadata["createdAt"] = str(current_time)
-                metadata["hash"] = blog_content_file_hash
-                metadata["id"] = str(uuid.uuid4())
 
-                os.makedirs(os.path.join(PATH_TO_STATIC, metadata["id"]))
-
-                with open(blog_content_path, 'r') as blog_content_file:
-                    blog_content = blog_content_file.read()
-                    processed_blog_content_file_path = os.path.join(PATH_TO_STATIC, metadata["id"],
-                                                                    str(metadata["id"]) + ".mdx")
-                    with open(processed_blog_content_file_path, 'w') as processed_blog_content_file:
-                        processed_blog_content_file.write(blog_content)
-
-                json.dump(metadata, open(blog_metadata_path, "w"), ensure_ascii=False, indent=4, sort_keys=True)
-                processed_metadata_file_path = os.path.join(PATH_TO_STATIC, metadata["id"], metadata["id"] + ".json")
-
-                json.dump(metadata, open(processed_metadata_file_path, "w"), ensure_ascii=False, indent=4, sort_keys=True)
+main()
